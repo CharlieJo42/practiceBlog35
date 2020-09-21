239594492fed7f13714fbc4da658da446223e395
Charlie Jo <charliejames1652@gmail.com>
2020-09-21 10:42:54 -0400

    added ability to edit

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -9,16 +9,30 @@ class ArticlesController < ApplicationController
     end
    
     def new
+        @article = Article.new
     end
+
+    def edit 
+        @article = Article.find(params[:id])
+    end
+
 
     def create
     #     render plain: params[:article].inspect
     # @article = Article.new(params[:article])
     # @article = Article.new(params.require(:article).permit(:title, :text))
+        # @article = Article.new(article_params)
+
+        # @article.save
+        # redirect_to @article
+
         @article = Article.new(article_params)
 
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
 
     private
